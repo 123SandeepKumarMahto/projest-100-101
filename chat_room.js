c08aac1f37594fb3041f97027354a76a57a9fe6e
123SandeepKumarMahto <106820377+123SandeepKumarMahto@users.noreply.github.com>
2022-10-07 18:47:36 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyApWwQH5CE7Ze6Fio88wPYBy6dGgelCskE",
+    authDomain: "kwitter-89020.firebaseapp.com",
+    databaseURL: "https://kwitter-89020-default-rtdb.firebaseio.com",
+    projectId: "kwitter-89020",
+    storageBucket: "kwitter-89020.appspot.com",
+    messagingSenderId: "902837684514",
+    appId: "1:902837684514:web:a899e734ebcff033b0bd7b"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
